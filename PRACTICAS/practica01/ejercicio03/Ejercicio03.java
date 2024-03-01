73190a01d5c7feb5907df36c4af2988c76f61de1
AdrianOrdonez2705 <fabriadri2705@gmail.com>
2024-02-29 21:47:21 -0400

    Desarrollo de Algoritmo en la función

--- PRACTICAS/practica01/ejercicio03/Ejercicio03.java
+++ PRACTICAS/practica01/ejercicio03/Ejercicio03.java
@@ -16,12 +16,26 @@ public class Ejercicio03 {
         System.out.println("Ingrese el segundo numero: ");
         Scanner scanner2 = new Scanner(System.in);
         int numero2 = scanner2.nextInt();
+        
+        //Algoritmo
+        int sumador = 0;
 
+        for(int i=1; i < numero1; i++){
+            if(numero1 % i == 0){
+                sumador += i;
+            }
+        }
+
+        if(sumador == numero2){
+            System.out.println(numero1 + " y " + numero2 + " SI son numeros amigos");
+        }
+        else{
+            System.out.println(numero1 + " y " + numero2 + " NO son numeros amigos");
+        }
     }
 
     public static void main(String[] args) {
         numerosAmigos();
     }
-
-
+    
 }

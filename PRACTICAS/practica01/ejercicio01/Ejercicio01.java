d480748aded36a2a2df55512b0987bd5a0f31174
AdrianOrdonez2705 <fabriadri2705@gmail.com>
2024-03-03 18:35:10 -0400

    Funciones para ingresar texto y para el menu

--- PRACTICAS/practica01/ejercicio01/Ejercicio01.java
+++ PRACTICAS/practica01/ejercicio01/Ejercicio01.java
@@ -4,15 +4,50 @@ import java.util.Scanner;
 
 public class Ejercicio01{
 
-
     // Menú de opciones
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        
+
         int opcion = 0;
 
+        String texto = "";
+
         do{
+            menu();
+
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(opcion){
+                case 1:
+                    ingresarTexto(scanner);
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                default:
+
+                    break;
+            }
+
+        } while(opcion != 6);
+
+        scanner.close();
+        
+    }
+
+    // mostrar menu
+    public static void menu(){
 
         System.out.println("\tMENU DE OPCIONES");
         System.out.println("1. Ingresar texto");
@@ -22,32 +57,12 @@ public class Ejercicio01{
         System.out.println("5. Reemplazar una palabra por otra");
         System.out.println("OTRO -> SALIR");
 
-        opcion = scanner.nextInt();
-
-        switch(opcion){
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            default:
-            
-                break;
-        }
-
-        } while(opcion != 6);
-
-        scanner.close();
     }
-    
+
+    // Ingresar texto
+    public static String ingresarTexto(Scanner scanner){
+        System.out.println("Ingrese texto: ");
+        return scanner.nextLine();
+    }
+
 }

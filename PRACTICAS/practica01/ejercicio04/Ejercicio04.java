cf3d275305965c048a510cde4b2d5ce6fe2dd069
AdrianOrdonez2705 <fabriadri2705@gmail.com>
2024-03-03 21:51:41 -0400

    Funcion para agregar deuda

--- PRACTICAS/practica01/ejercicio04/Ejercicio04.java
+++ PRACTICAS/practica01/ejercicio04/Ejercicio04.java
@@ -8,10 +8,20 @@ public class Ejercicio04 {
         
         int opcion = 0;
 
+        float deuda = 0;
         do{
             menu();
             opcion = scanner.nextInt();
             scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    deuda = agregarDeuda(deuda,scanner);
+                    break;
+            
+                case 5:
+                    break;
+            }
 
         } while(opcion != 5);
 
@@ -27,6 +37,32 @@ public class Ejercicio04 {
         System.out.println("4. Desplegar 'Estado de Cuentas'");
         System.out.println("5. SALIR");
         System.out.println("Ingrese una opcion: ");
+
+    }
+
+    public static float agregarDeuda(float deuda, Scanner scanner){
+        float[] UVE = {1164, 2910, 3492, 4074, 8730};
+
+        System.out.println("Nro de materias UVE 2: ");
+        float N_uve_2 = scanner.nextInt();
+
+        System.out.println("Nro de materias UVE 5: ");
+        float N_uve_5 = scanner.nextInt();
+
+        System.out.println("Nro de materias UVE 6: ");
+        float N_uve_6 = scanner.nextInt();
+
+        System.out.println("Nro de materias UVE 7: ");
+        float N_uve_7 = scanner.nextInt();
+
+        System.out.println("Nro de materias UVE 15: ");
+        float N_uve_15 = scanner.nextInt();
+
+        deuda = (N_uve_2 * UVE[0]) + (N_uve_5 * UVE[1]) + (N_uve_6 * UVE[2]) + (N_uve_7 * UVE[3]) + (N_uve_15 * UVE[4]);
+
+        System.out.println("Su deuda (semestral) es de Bs: " + deuda + "\n");
+
+        return deuda;
 
     }
 }
